f635848d77d7ab135719c63b7afb33d1c545c737
jjspielhurt <silviu.andrei53m@gmail.com>
2020-08-29 00:17:59 +0300

    mail try

--- business-start/mail/contact_me.php
+++ business-start/mail/contact_me.php
@@ -1,5 +1,4 @@
 <?php
-// Check for empty fields
 if(empty($_POST['name'])      ||
    empty($_POST['email'])     ||
    empty($_POST['phone'])     ||
@@ -9,18 +8,82 @@ if(empty($_POST['name'])      ||
    echo "No arguments Provided!";
    return false;
    }
-   
+
 $name = strip_tags(htmlspecialchars($_POST['name']));
 $email_address = strip_tags(htmlspecialchars($_POST['email']));
 $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
    
-// Create the email and send the message
-$to = 'silviu.andrei53m@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Mesaj pe Site de la:  $name";
-$email_body = "Detalii:\n\nNume: $name\n\nEmail: $email_address\n\nTelefon: $phone\n\nMesaj:\n$message";
-$headers = "From: cherieboo12@gmail.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";   
-mail($to,$email_subject,$email_body,$headers);
-return true;         
+
+//SMTP needs accurate times, and the PHP time zone MUST be set
+//This should be done in your php.ini, but this is how to do it if you don't have access to that
+date_default_timezone_set('Etc/UTC');
+
+require './phpmailer/PHPMailerAutoload.php';
+
+//Create a new PHPMailer instance
+$mail = new PHPMailer;
+
+//Tell PHPMailer to use SMTP
+$mail->isSMTP();
+
+//Enable SMTP debugging
+// 0 = off (for production use)
+// 1 = client messages
+// 2 = client and server messages
+$mail->SMTPDebug = 2;
+
+//Ask for HTML-friendly debug output
+$mail->Debugoutput = 'html';
+
+//Set the hostname of the mail server
+$mail->Host = 'smtp.gmail.com';
+// use
+// $mail->Host = gethostbyname('smtp.gmail.com');
+// if your network does not support SMTP over IPv6
+
+//Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
+$mail->Port = 587;
+
+//Set the encryption system to use - ssl (deprecated) or tls
+$mail->SMTPSecure = 'tls';
+
+//Whether to use SMTP authentication
+$mail->SMTPAuth = true;
+
+//Username to use for SMTP authentication - use full email address for gmail
+$mail->Username = "noreply.tas.depozit@gmail.com";
+
+//Password to use for SMTP authentication
+$mail->Password = "L123rnj9U3sl";
+
+//Set who the message is to be sent from
+$mail->setFrom($email_address, 'First Last');
+
+// //Set an alternative reply-to address
+// $mail->addReplyTo('replyto@example.com', 'First Last');
+
+//Set who the message is to be sent to
+$mail->addAddress('silviu.andrei53m@gmail.com', 'Silviu M.');
+
+//Set the subject line
+$mail->Subject = "Mesaj pe Site de la:  $name";;
+
+//Read an HTML message body from an external file, convert referenced images to embedded,
+// //convert HTML into a basic plain-text alternative body
+// $mail->msgHTML(file_get_contents('contents.html'), dirname(__FILE__));
+
+//Replace the plain text body with one created manually
+$mail->AltBody = 'This is a plain-text message body';
+
+//Attach an image file
+// $mail->addAttachment('images/phpmailer_mini.png');
+
+//send the message, check for errors
+if (!$mail->send()) {
+    echo "Mailer Error: " . $mail->ErrorInfo;
+} else {
+    echo "Message sent!";
+}
+
 ?>
