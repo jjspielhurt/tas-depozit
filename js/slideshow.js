3eb859ae515584b2159e113145afa0cfc9307abc
jjspielhurt <silviu.andrei53m@gmail.com>
2020-09-10 19:12:47 +0300

    slideshow img v1

--- js/slideshow.js
+++ js/slideshow.js
@@ -12,6 +12,6 @@ function carousel() {
     x[x.length - 1].style.display = "block";
   } else {
     x[myIndex - 1].style.display = "block";
-    setTimeout(carousel, 400);
+    setTimeout(carousel, 800);
   } // Change image every 2 seconds
 }
