41d6145ed0aa4c77ea6d1470f6a58052b9a23e8a
jjspielhurt <silviu.andrei53m@gmail.com>
2020-09-14 20:15:07 +0300

    faster navbar transition

--- js/navbarTransition.js
+++ js/navbarTransition.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
   // Transition effect for navbar
   $(window).scroll(function () {
     // checks if window is scrolled more than 800px, adds/removes solid class
-    if ($(this).scrollTop() > 800) {
+    if ($(this).scrollTop() > 650) {
       $(".navbar").addClass("solid");
       $(".navbar-collapse").addClass("solid");
     } else {
