089baa2ee2749d6bc9b6d534ef70d962352239fb
jjspielhurt <silviu.andrei53m@gmail.com>
2020-09-23 14:39:28 +0300

    final offer banner

--- js/animate_product_card.js
+++ js/animate_product_card.js
@@ -3,8 +3,8 @@ $(document).ready(function () {
   $(window).scroll(function () {
     // checks if window is scrolled more than 800px, adds/removes solid class
     if ($("#animatedCard").is(":visible")) {
-      $("#animatedCard").delay(300).addClass("animate");
-      setTimeout(() => $("#animatedCard").removeClass("animate"), 1000);
+      $("#animatedCard").delay(1000).addClass("animate");
+      setTimeout(() => $("#animatedCard").removeClass("animate"), 4000);
     } else {
     }
   });
