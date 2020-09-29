115d2cf9d1a4081930cd4cc4f083c6bd57728310
jjspielhurt <silviu.andrei53m@gmail.com>
2020-09-29 13:50:25 +0300

    working x button fix bugs

--- js/navbar_button_transition.js
+++ js/navbar_button_transition.js
@@ -1,12 +1,14 @@
 menuBtn = document.getElementById("menu-btn");
 menuBtn.addEventListener("click", function () {
-  // show x button
-  if (document.getElementById("navbar-close").classList.contains("hidden")) {
-    $("#navbar-hamburger").addClass("hidden");
-    $("#navbar-close").removeClass("hidden");
-  } else {
-    // show hamburger
-    $("#navbar-hamburger").removeClass("hidden");
-    $("#navbar-close").addClass("hidden");
-  }
+  setTimeout(function () {
+    // show x button
+    if (!document.getElementById("menu-btn").classList.contains("collapsed")) {
+      $("#navbar-hamburger").addClass("hidden");
+      $("#navbar-close").removeClass("hidden");
+    } else {
+      // show hamburger
+      $("#navbar-hamburger").removeClass("hidden");
+      $("#navbar-close").addClass("hidden");
+    }
+  }, 100);
 });
