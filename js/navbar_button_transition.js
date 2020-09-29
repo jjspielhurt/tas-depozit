02103c021a5b75c4c8568d7dceff5dcb1045dad0
jjspielhurt <silviu.andrei53m@gmail.com>
2020-09-29 13:38:48 +0300

    working x button

--- js/navbar_button_transition.js
+++ js/navbar_button_transition.js
@@ -1,11 +1,12 @@
-$(function () {
-  $("#navbarResponsive")
-    .on("shown.bs.collapse", function () {
-      $("#navbar-hamburger").addClass("hidden");
-      $("#navbar-close").removeClass("hidden");
-    })
-    .on("hidden.bs.collapse", function () {
-      $("#navbar-hamburger").removeClass("hidden");
-      $("#navbar-close").addClass("hidden");
-    });
+menuBtn = document.getElementById("menu-btn");
+menuBtn.addEventListener("click", function () {
+  // show x button
+  if (document.getElementById("navbar-close").classList.contains("hidden")) {
+    $("#navbar-hamburger").addClass("hidden");
+    $("#navbar-close").removeClass("hidden");
+  } else {
+    // show hamburger
+    $("#navbar-hamburger").removeClass("hidden");
+    $("#navbar-close").addClass("hidden");
+  }
 });
