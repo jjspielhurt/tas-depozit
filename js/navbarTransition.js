19623bb6e3dcde7a90eda6e6df73ed156bd4c172
jjspielhurt <silviu.andrei53m@gmail.com>
2020-10-02 14:29:01 +0300

    offer on same parallax as products

--- js/navbarTransition.js
+++ js/navbarTransition.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
   // Transition effect for navbar
   $(document.getElementById("parallax")).scroll(function () {
     // checks if window is scrolled more than 800px, adds/removes solid class
-    if ($(this).scrollTop() > 650) {
+    if ($(this).scrollTop() > 590) {
       $(".navbar").addClass("solid");
       $(".navbar-collapse").addClass("solid");
     } else {
